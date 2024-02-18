a5527537058bbf8bfead07d1a06a7bf8711fff80
Maxim Ryabykh <ryabykhms@gmail.com>
2024-02-18 15:59:06 +0300

    initialized base hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
+
+export default config;
